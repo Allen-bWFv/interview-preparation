6d4e4e2375016d0f684d103eb92d67b82a5687c4
Allen-bWFv <1543684485@qq.com>
2020-10-19 22:28:10 +0800

    fix:完成两数相加

--- src/com/mzy/leecode/TwoPlus.java
+++ src/com/mzy/leecode/TwoPlus.java
@@ -3,12 +3,15 @@ package com.mzy.leecode;
 public class TwoPlus {
 
     public static void main(String[] args) {
-        ListNode l1 = new ListNode(2);
-        l1.next = new ListNode(4);
-        l1.next.next = new ListNode(3);
-        ListNode l2 = new ListNode(5);
-        l2.next = new ListNode(6);
-//        l2.next.next = new ListNode(4);
+        ListNode l1 = new ListNode(9);
+        l1.next = new ListNode(9);
+        l1.next.next = new ListNode(9);
+        l1.next.next.next = new ListNode(9);
+        l1.next.next.next.next = new ListNode(9);
+        l1.next.next.next.next.next = new ListNode(9);
+        ListNode l2 = new ListNode(7);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(9);
         ListNode listNode = addTwoNumbers(l1, l2);
         System.out.println(listNode);
     }
@@ -24,6 +27,15 @@ public class TwoPlus {
                 }else {
                     l1.next.val = l1.next.val+1;
                 }
+            }
+            if (l2.next == null){
+                if (l1.next != null && l1.next.val >= 10){
+                    addTwoNumbers(l1.next, new ListNode(0));
+                }
+                return l1;
+            }
+            if (l1.next == null){
+                l1.next = new ListNode(0);
             }
             addTwoNumbers(l1.next, l2.next);
         }
